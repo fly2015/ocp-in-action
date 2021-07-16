6b2b4c4195d8edfc6f4d3e03b88b32ca3a3a07f7
Fly2020 <fr.quochien@gmail.com>
2021-07-16 09:36:00 +0700

    generic exercise

--- src/main/java/java21/com/generic/doc/QuestionsAndExercises.java
+++ src/main/java/java21/com/generic/doc/QuestionsAndExercises.java
@@ -7,6 +7,11 @@
  */
 package java21.com.generic.doc;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+
 /**
  * 
  *
@@ -15,7 +20,50 @@ package java21.com.generic.doc;
  */
 public class QuestionsAndExercises
 {
+	static int countOldNumber(Collection<? extends Integer> numbers) {
+		int count = 0;
+		Iterator<? extends Integer> iterator = numbers.iterator();
+		while (iterator.hasNext()) {
+			Integer next = iterator.next();
+			if(next != null && next.intValue() % 2 != 0)
+			{
+				count = count + 1;
+			}
+		}
+		
+		return count;
+	}
+	
+	public static <T> T max(T x, T y) {
+		Comparator<T> comparator = new Comparator<T>() {
 
+			@Override
+			public int compare(T o1, T o2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		};
+		
+		int compare = comparator.compare(x, y);
+        return compare > 0 ? x : y;
+    }
+	
+	public static void main(String[] args) {
+		
+		// 1
+		Collection<Integer> cNumbers = new ArrayList<Integer>();
+		cNumbers.add(1);
+		cNumbers.add(2);
+		cNumbers.add(3);
+		cNumbers.add(4);
+		cNumbers.add(5);
+		
+		System.out.println(countOldNumber(cNumbers));
+		
+		//2 -> No
+		max(4, 5);
+		
+	}
 }
 
 
