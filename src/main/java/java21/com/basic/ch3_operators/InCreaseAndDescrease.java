ee392554cc27bcc21a061a024134492e648ea1df
Hien Ng <fr.quochien@gmail.com>
2022-04-21 10:34:35 +0700

    continue learning

---
+++ src/main/java/java21/com/basic/ch3_operators/InCreaseAndDescrease.java
@@ -0,0 +1,32 @@
+/*
+ * InCreaseAndDescrease.java
+ *
+ * Copyright by CRIF AG
+ * Zürich
+ * All rights reserved.
+ */
+package java21.com.basic.ch3_operators;
+
+/**
+ * 
+ *
+ * @author nhqhien
+ * @version $Revision:  $
+ */
+public class InCreaseAndDescrease
+{
+    public static void main(String[] args)
+    {
+        int lion = 3;
+        int tiger = ++lion * 5 / lion--;
+        System.out.println("lion is " + lion);
+        System.out.println("tiger is " + tiger);
+    }
+}
+
+
+
+/*
+ * Changes:
+ * $Log: $
+ */
---
+++ src/main/java/java21/com/basic/ch4_making_decisions/ModifyingLoopVariables.java
@@ -0,0 +1,29 @@
+/*
+ * ModifyingLoopVariables.java
+ *
+ * Copyright by CRIF AG
+ * Zürich
+ * All rights reserved.
+ */
+package java21.com.basic.ch4_making_decisions;
+
+/**
+ * 
+ *
+ * @author nhqhien
+ * @version $Revision:  $
+ */
+public class ModifyingLoopVariables
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
+
+
+
+/*
+ * Changes:
+ * $Log: $
+ */
