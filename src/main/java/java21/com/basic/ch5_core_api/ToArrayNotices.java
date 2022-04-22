f9bc78a1efa384569549d9bf2f12e45a4ccda3d3
Hien Ng <fr.quochien@gmail.com>
2022-04-22 17:10:01 +0700

    continue learning

---
+++ src/main/java/java21/com/basic/ch5_core_api/ToArrayNotices.java
@@ -0,0 +1,38 @@
+/*
+ * ToArrayNotices.java
+ *
+ * Copyright by CRIF AG
+ * Zürich
+ * All rights reserved.
+ */
+package java21.com.basic.ch5_core_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 
+ *
+ * @author nhqhien
+ * @version $Revision:  $
+ */
+public class ToArrayNotices
+{
+     public static void main(String[] args)
+    {
+         List<String> list1 = Arrays.asList("one", "two");
+         
+         Object[] objectArray = list1.toArray();
+         String[] stringArray = list1.toArray(new String[1]);
+         
+         System.out.println(Arrays.toString(stringArray));
+         System.out.println(stringArray.length);
+    }   
+}
+
+
+
+/*
+ * Changes:
+ * $Log: $
+ */
---
+++ src/main/java/java21/com/basic/ch6_lamdas_functional/LamdasDeclareVariableTrick.java
@@ -0,0 +1,37 @@
+/*
+ * LamdasDeclareVariableTrick.java
+ *
+ * Copyright by CRIF AG
+ * Zürich
+ * All rights reserved.
+ */
+package java21.com.basic.ch6_lamdas_functional;
+
+
+/**
+ * 
+ *
+ * @author nhqhien
+ * @version $Revision:  $
+ */
+public class LamdasDeclareVariableTrick
+{
+    public static void main(String[] a)
+    {
+        /*
+         * int b = 1;
+         * Predicate<Integer> p1 = a -> {
+         * int b = 0;
+         * int c = 0;
+         * return b == c;
+         * };
+         */
+    }
+}
+
+
+
+/*
+ * Changes:
+ * $Log: $
+ */
